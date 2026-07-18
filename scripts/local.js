3efa5c6d56c326ca14124c21327a6d4aa046d6eb
kjsgm <ksotelogmontanhaur@byupathway.edu>
2026-07-18 09:46:25 -0300

    Semana3

---
+++ scripts/local.js
@@ -0,0 +1,39 @@
+// Atualiza o ano no rodapé
+const currentYear = new Date().getFullYear();
+
+document.querySelector("#currentyear").textContent = currentYear;
+
+
+// Mostra a última atualização da página
+const lastModified = document.lastModified;
+
+document.querySelector("#lastModified").textContent =
+    `Última atualização: ${lastModified}`;
+
+
+// Calcula a sensação térmica
+const temperature = 18;
+const windSpeed = 10;
+
+function calculateWindChill(temperature, windSpeed) {
+
+    return (
+        13.12 +
+        0.6215 * temperature -
+        11.37 * Math.pow(windSpeed, 0.16) +
+        0.3965 * temperature * Math.pow(windSpeed, 0.16)
+    ).toFixed(1);
+}
+
+
+if (temperature <= 10 && windSpeed > 4.8) {
+
+    document.querySelector("#windchill").textContent =
+        `${calculateWindChill(temperature, windSpeed)} °C`;
+
+} else {
+
+    document.querySelector("#windchill").textContent =
+        "Não disponível";
+
+}
