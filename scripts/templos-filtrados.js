00490bf9a328dbfdd845d7ca8104d9732074ce8e
kjsgm <ksotelogmontanhaur@byupathway.edu>
2026-07-25 19:47:09 -0300

    ajuste

--- scripts/templos-filtrados.js
+++ scripts/templos-filtrados.js
@@ -35,70 +35,70 @@ const templos = [
         location: "Aba, Nigéria",
         dedicated: "2005, August, 7",
         area: 11500,
-        imageUrl: "images/templos/small/aba-nigeria-temple.webp"
+        imageUrl: "imagens/templos/small/aba-nigeria-temple.webp"
     },
     {
         templeName: "Manti Utah",
         location: "Manti, Utah, Estados Unidos",
         dedicated: "1888, May, 21",
         area: 74792,
-        imageUrl: "images/templos/small/manti-utah-temple.webp"
+        imageUrl: "imagens/templos/small/manti-utah-temple.webp"
     },
     {
         templeName: "Payson Utah",
         location: "Payson, Utah, Estados Unidos",
         dedicated: "2015, June, 7",
         area: 96630,
-        imageUrl: "images/templos/small/payson-utah-temple.webp"
+        imageUrl: "imagens/templos/small/payson-utah-temple.webp"
     },
     {
         templeName: "Yigo Guam",
         location: "Yigo, Guam",
         dedicated: "2020, May, 2",
         area: 6861,
-        imageUrl: "images/templos/small/yigo-guam-temple.webp"
+        imageUrl: "imagens/templos/small/yigo-guam-temple.webp"
     },
     {
         templeName: "Washington D.C.",
         location: "Kensington, Maryland, Estados Unidos",
         dedicated: "1974, November, 19",
         area: 156558,
-        imageUrl: "images/templos/small/washington-d.c.-temple.webp"
+        imageUrl: "imagens/templos/small/washington-d.c.-temple.webp"
     },
     {
         templeName: "Lima Peru",
         location: "Lima, Peru",
         dedicated: "1986, January, 10",
         area: 9600,
-        imageUrl: "images/templos/small/lima-peru-temple.webp"
+        imageUrl: "imagens/templos/small/lima-peru-temple.webp"
     },
     {
         templeName: "Mexico City Mexico",
         location: "Cidade do México, México",
         dedicated: "1983, December, 2",
         area: 116642,
-        imageUrl: "images/templos/small/mexico-city-mexico-temple.webp"
+        imageUrl: "imagens/templos/small/mexico-city-mexico-temple.webp"
     },
     {
         templeName: "Campinas Brasil",
         location: "Campinas, São Paulo, Brasil",
         dedicated: "2002, May, 17",
         area: 48100,
-        imageUrl: "images/templos/small/campinas-brazil-temple.webp"
+        imageUrl: "imagens/templos/small/campinas-brazil-temple.webp"
     },
     {
         templeName: "Arequipa Peru",
         location: "Arequipa, Peru",
         dedicated: "2019, December, 15",
         area: 9300,
-        imageUrl: "images/templos/small/arequipa-peru-temple.webp"
+        imageUrl: "imagens/templos/small/arequipa-peru-temple.webp"
     },
     {
         templeName: "Fortaleza Brasil",
         location: "Fortaleza, Ceará, Brasil",
         dedicated: "2019, June, 2",
         area: 36000,
-        imageUrl: "images/templos/small/fortaleza-brazil-temple.webp"
+        imageUrl: "imagens/templos/small/fortaleza-brazil-temple.webp"
     }
 ];
 
